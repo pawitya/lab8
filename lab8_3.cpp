9663ca050b363719fd3a53f80a5dc9effd467644
pawitya <pawitchaya_jaijumpa@cmu.ac.th>
2024-12-12 22:11:08 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,48 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age, height;
+    long long bounty;
+    string character;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age > 60) {
+        character = "Brook";
+    } else if (age > 25) {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+
+        if (bounty > 500000000) {
+            character = "Jinbe";
+        } else {
+            character = "Franky";
+        }
+    } else {
+       
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if (height < 100) {
+            character = "Chopper";
+        } else if (height < 180) {
+            character = "Usopp";
+        } else {
+
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+
+            if (bounty > 1100000000) {
+                character = "Zoro";
+            } else {
+                character = "Sanji";
+            }
+        }
+    }
+
+    cout << "Your character = " << character << endl;
+
+    return 0;
+}
